efa6d0978f59eaca3da4573b994efcb6b323178d
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-09-27 16:58:00 +1000

    Finishes FizzBuzz functionality and adds palindrome functionality

--- public/js/fizz.js
+++ public/js/fizz.js
@@ -1,19 +1,24 @@
 const first = document.querySelector("#first")
 const second = document.querySelector("#second")
+const result = document.querySelector("#fb-result")
 
 document.addEventListener("submit", (e) => {
     e.preventDefault()
+    while(result.firstChild){
+        result.removeChild(result.firstChild)
+    }
     fizzBuzz(parseInt(first.value), parseInt(second.value))
 })
 
 const fizzBuzz = (x, y) => {
+    document.querySelector(".errors").textContent = ""
     // Error handling
     if(x > y){
-        return document.querySelector("#fb-num-1").textContent = "First number must be less than the second"
+        return document.querySelector(".errors").textContent = "First number must be less than the second"
     } else if (x < 0 || x > 100){
-        return document.querySelector("#fb-num-1").textContent = "Number must be between 1 and 100"
+        return document.querySelector(".errors").textContent = "Number must be between 1 and 100"
     } else if (y < 0 || y > 100){
-        return document.querySelector("#fb-num-2").textContent = "Number must be between 1 and 100"
+        return document.querySelector(".errors").textContent = "Number must be between 1 and 100"
     }
     
     // Loop for maths
@@ -28,7 +33,7 @@ const fizzBuzz = (x, y) => {
         } else {
             item.textContent = i
         }
-        document.querySelector("#fb-result").append(item)
+        result.append(item)
     }
     first.value = ''
     second.value = ''
